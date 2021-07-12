ff6afd206359789abafe6d46357194e7135d8ae2
miltondw <estradamilton2001@gmail.com>
2021-07-12 14:59:25 -0500

    Header part one finished version-1

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,14 +1,40 @@
 import Link from "next/link";
+import { useState } from "react";
 import Items from "./Items.json";
+import { useRouter } from "next/router";
 export default function Navbar() {
+  const router = useRouter();
+  const [nav, setNav] = useState(false);
+  const isActivo = (r) => {
+    if (r === router.pathname) {
+      return "active";
+    } else {
+      return "";
+    }
+  };
+
   return (
     <header className="header">
+      <div className="menu">
+        <h1 className="logo">
+          <Link href="/">
+            <a>MiltonDw</a>
+          </Link>
+        </h1>
+        <div onClick={() => setNav(!nav)} className="toggle-menu">
+          <div className={`${nav ? "show" : ""} first-bar`}></div>
+          <div className={`${nav ? "show" : ""} second-bar`}></div>
+          <div className={`${nav ? "show" : ""} third-bar`}></div>
+        </div>
+      </div>
       <nav className="navbar">
-        <ul className="navbar-content">
+        <ul className={`navbar-content ${nav ? "show" : ""}`}>
           {Items.map((item) => (
             <li key={item.id} className="navbar-content__item">
               <Link href={item.link}>
-                <a className="navbar-content__link">{item.name}</a>
+                <a className={`navbar-content__link ${isActivo(item.link)}`}>
+                  {item.name}
+                </a>
               </Link>
             </li>
           ))}

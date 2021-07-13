530c9f94c0c3705a43871808370cdaf1d795856c
miltondw <estradamilton2001@gmail.com>
2021-07-13 14:19:45 -0500

    Created component about

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layaut from "../components/Template/Layaut";
 import Head from "next/head";
 import Home from "../components/Home/Home";
 import Services from "../components/Services/Services";
+import About from "../components/About/About";
 export default function App() {
   return (
     <Layaut>
@@ -10,6 +11,7 @@ export default function App() {
       </Head>
       <Home />
       <Services />
+      <About />
     </Layaut>
   );
 }

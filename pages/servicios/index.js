d084abed172bb008e1d4ca33dda0dd2574348ef3
miltondw <estradamilton2001@gmail.com>
2021-07-17 17:06:29 -0500

    Modification config colors and fonts

---
+++ pages/servicios/index.js
@@ -0,0 +1,26 @@
+import Layaut from "../../components/Template/Layaut";
+
+export default function Servicios() {
+  return (
+    <Layaut>
+      <div id="consultoria">
+        <h2>consultoria</h2>
+      </div>
+      <div id="organizacion-y-logistica">
+        <h2>organizacion-y-logistica</h2>
+      </div>
+      <div id="landing-page">
+        <h2>landing-page</h2>
+      </div>
+      <div id="web-app">
+        <h2>web-app</h2>
+      </div>
+      <div id="tienda-online">
+        <h2>tienda-online</h2>
+      </div>
+      <div id="digital-marketing">
+        <h2>digital-marketing</h2>
+      </div>
+    </Layaut>
+  );
+}

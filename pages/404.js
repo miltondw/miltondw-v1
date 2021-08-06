133bd962acd1b407203c5d32ac8a42e984f99a58
miltondw <estradamilton2001@gmail.com>
2021-08-06 10:16:56 -0500

    Page 404 created

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layaut from "../components/Template/Layaut";
+export default function Custom404() {
+  return (
+    <Layaut>
+      <div className="error404__title">
+        <h2 className="Title">Error -404- Página no Encontrada</h2>
+      </div>
+      <div className="error404"></div>
+    </Layaut>
+  );
+}

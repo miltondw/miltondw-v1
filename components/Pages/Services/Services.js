54f43633793bb48ff42480d2061784380bcac860
miltondw <estradamilton2001@gmail.com>
2021-11-05 11:46:38 -0500

    mejorar el performance

--- components/Pages/Services/Services.js
+++ components/Pages/Services/Services.js
@@ -26,13 +26,13 @@ export default function Services() {
 
   //Particles
   const particlesInit = (main) => {
-    console.log(main);
+    // console.log(main);
 
     // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
 
   const particlesLoaded = (container) => {
-    console.log(container);
+    // console.log(container);
   };
   return (
     <div id="servicios" className="services-home" ref={cardService}>
--- pages/servicios/index.js
+++ pages/servicios/index.js
@@ -197,13 +197,13 @@ export default function Servicios() {
   }, []);
   //Particles
   const particlesInit = (main) => {
-    console.log(main);
+    // console.log(main);
 
     // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
 
   const particlesLoaded = (container) => {
-    console.log(container);
+    // console.log(container);
   };
   return (
     <Layaut>

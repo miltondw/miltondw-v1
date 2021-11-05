0649798a191c8c14a9ee9f68ee4b1a5746ec94af
miltondw <estradamilton2001@gmail.com>
2021-11-05 11:06:53 -0500

    animation particles

--- components/Pages/Services/Services.js
+++ components/Pages/Services/Services.js
@@ -1,5 +1,6 @@
 import CardServices from "./CardServices";
 import services from "../../../db/services.json";
+import Particles from "react-tsparticles";
 //Animation
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
@@ -22,8 +23,101 @@ export default function Services() {
       // return console.log(element.querySelector(`#card-${s.id}`));
     });
   }, []);
+
+  //Particles
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div id="servicios" className="services-home" ref={cardService}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#01010100",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <h2 className="Title" id="titleServices">
         Servicios
       </h2>
